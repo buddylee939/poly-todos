84ec9bff68a07b74829ca45ad9220fb5d5f428d8
Jose Merconchini <buddylee939@hotmail.com>
2021-11-29 10:01:21 -0500

    Updated devise links and layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  def username
+    email.split('@')[0]
+  end
 end
